0809ea992125ef0e89ecd277a68c24280fb1c8c3
Harsh Sinha <harsh4723@rediffmail.com>
2021-01-18 17:00:55 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb+srv://harsh:qwerty123@cluster0.9lnr9.mongodb.net/test",
   "bodyLimit": "100kb"
 }
